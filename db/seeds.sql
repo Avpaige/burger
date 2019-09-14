70e4ad5efbc283916afaeb05cc7644ea27d4ccae
Ari Virgil-Paige <avpaige@gmail.com>
2019-09-14 12:43:27 -0700

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("American Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bleu Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portebella Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Philly Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalepeno Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Korean BBQ Burger", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", False);
+
+
